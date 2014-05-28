1dc45351f59bafefb9e7a12ab3887800a29d1ff4
Sean G Wilbur <sgwilbur@gmail.com>
2014-05-28 15:25:35 +1000

    Updated to a slightly nicer version of the start script.

--- start_engine0.sh
+++ start_engine0.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
+# Helper script to run a build engine.
+# @sgwilbur
+#
+# todo:
+#  Add runas user
+# check if directories exist
 
-TS=`date '+%Y%m%d-%H%M'`
-JBE=/opt/IBM/RTC/buildsystem/buildengine/eclipse/jbe
-WORK_DIR=/opt/jbe
-LOG_DIR=${WORK_DIR}/logs
+ts=`date '+%Y%m%d-%H%M'`
 
+jbe_install_dir="/opt/IBM/TeamConcertBuild"
+jbe_dir="${jbe_install_dir}/buildsystem/buildengine/eclipse"
+jbe_toolkit="${jbe_install_dir}/buildsystem/buildtoolkit"
+jbe="${jbe_dir}/jbe"
 
-repositoryAddress=https://hub.jazz.net/ccm01/
-userId=swilbur
-engineId=swilbur.engine0
-engine_dir=${WORK_DIR}/${engineId}
-passwordFile=${WORK_DIR}/swilbur.jazzhub
-log_file=${LOG_DIR}/${engineId}-${TS}.log
+work_dir="/var/ibm/jbe"
+repositoryAddress="https://server/ccm"
+userId="build"
+passwordFile="${work_dir}/build.pwd"
+engineId="jke.dev.engine"
 
+engine_work_dir="${work_dir}/${engineId}"
+log_file="${engine_work_dir}/${engineId}-${ts}.log"
 
-CMD="${JBE} -repository ${repositoryAddress} -userId ${userId} -passwordFile ${passwordFile} -engineId ${engineId}"
+CMD="${jbe} -repository ${repositoryAddress} -userId ${userId} -passwordFile ${passwordFile} -engineId ${engineId}"
 
-echo ${CMD}
-pushd ${engine_dir}
-${CMD} > ${log_file} &
+echo "Starting a new Build Engine process @ ${ts}" 	>  ${log_file}
+echo "\tWorking directory: ${engine_work_dir}  "	>> ${log_file}
+echo "\trepositoryAddres: ${repositoryAddress}"	 	>> ${log_file}
+mkdir -p ${engine_work_dir} 						>> ${log_file}
+pushd ${engine_work_dir}
+echo ${CMD} 										>> ${log_file}
+echo "############################################" >> ${log_file}
+${CMD} 												>> ${log_file} &
 popd
-
